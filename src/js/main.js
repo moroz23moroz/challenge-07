88a35943af5903ac793814ce68a0b6d8e1816aa1
Marina Moroz <moroz2387moroz@yandex.ru>
2024-06-04 19:55:51 +1000

    add script

--- src/js/main.js
+++ src/js/main.js
@@ -1,4 +1,18 @@
-window.addEventListener('DOMContentLoaded', () => {
-    console.log('start')
-})
+window.addEventListener("DOMContentLoaded", () => {
+  const left = document.querySelector(".left");
+  const right = document.querySelector(".right");
+  const container = document.querySelector(".container");
 
+  left.addEventListener("mouseenter", () =>
+    container.classList.add("hover-left")
+  );
+  left.addEventListener("mouseleave", () =>
+    container.classList.remove("hover-left")
+  );
+  right.addEventListener("mouseenter", () =>
+    container.classList.add("hover-right")
+  );
+  right.addEventListener("mouseleave", () =>
+    container.classList.remove("hover-right")
+  );
+});
